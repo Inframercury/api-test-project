47871d68dd9949891970f8e17f23bba96af9998e
Лукьянов Филипп Анатольевич <lukyanov.f.an@sberbank.ru>
2019-10-28 17:31:13 +0700

    Update api_tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,46 +1,54 @@
 import requests
 import logging
+from allure_commons._allure import step
+import pytest
 
 
 class TestApi:
 
     LOGGER = logging.getLogger(__name__)
 
-    """Тестирование endpoint http://httpbin.org/headers"""
+    @pytest.mark.api
     def test_headers(self):
-        link = "http://httpbin.org/headers"
-        TestApi.LOGGER.info("Sending request: " + link)
-        result = requests.get(link)
-        TestApi.LOGGER.info("Getting response: " + str(result.json()))
-        try:
-            #проверяем, что вернулся status_code 200
-            assert 200 == result.status_code
-        except AssertionError as err:
-            TestApi.LOGGER.error(err, exc_info=True)
-            raise err
+        """Тестирование endpoint http://httpbin.org/headers"""
+        with step("Тест заголовков"):
+            link = "http://httpbin.org/headers"
+            TestApi.LOGGER.info("Sending request: " + link)
+            result = requests.get(link)
+            TestApi.LOGGER.info("Getting response: " + str(result.json()))
+            try:
+                #проверяем, что вернулся status_code 200
+                assert 200 == result.status_code
+            except AssertionError as err:
+                TestApi.LOGGER.error(err, exc_info=True)
+                raise err
 
-    """Тестирование endpoint http://httpbin.org/status с параметром 200"""
+    @pytest.mark.api
     def test_status(self):
-        link = "http://httpbin.org/status/300"
-        TestApi.LOGGER.info("Sending request: " + link)
-        result = requests.get(link)
-        TestApi.LOGGER.info("Getting response: " + str(result.content))
-        try:
-            #проверяем, что вернулся status_code 200
-            assert 300 == result.status_code
-        except AssertionError as err:
-            TestApi.LOGGER.error(err, exc_info=True)
-            raise err
+        """Тестирование endpoint http://httpbin.org/status с параметром 300"""
+        with step("Тест статус кода"):
+            link = "http://httpbin.org/status/300"
+            TestApi.LOGGER.info("Sending request: " + link)
+            result = requests.get(link)
+            TestApi.LOGGER.info("Getting response: " + str(result.content))
+            try:
+                #проверяем, что вернулся status_code 200
+                assert 300 == result.status_code
+            except AssertionError as err:
+                TestApi.LOGGER.error(err, exc_info=True)
+                raise err
 
+    @pytest.mark.api
     def test_redirect(self):
         """Тестирование endpoint http://httpbin.org/redirect/ с параметром 1"""
-        link = "http://httpbin.org/redirect/1"
-        TestApi.LOGGER.info("Sending request: " + link)
-        result = requests.get(link)
-        TestApi.LOGGER.info("Getting response: " + str(result.json()))
-        try:
-            #проверяем, что вернулся status_code 302
-            assert 302 == result.status_code
-        except AssertionError as err:
-            TestApi.LOGGER.error(err, exc_info=True)
-            raise err
+        with step("Тест редиректа"):
+            link = "http://httpbin.org/redirect/1"
+            TestApi.LOGGER.info("Sending request: " + link)
+            result = requests.get(link)
+            TestApi.LOGGER.info("Getting response: " + str(result.json()))
+            try:
+                #проверяем, что вернулся status_code 302
+                assert 302 == result.status_code
+            except AssertionError as err:
+                TestApi.LOGGER.error(err, exc_info=True)
+                raise err
