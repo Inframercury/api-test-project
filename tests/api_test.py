701d0eafa302f677954e58f8444b10eff8083177
Лукьянов Филипп Анатольевич <lukyanov.f.an@sberbank.ru>
2019-10-28 16:52:18 +0700

    Modify api tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -6,23 +6,41 @@ class TestApi:
 
     LOGGER = logging.getLogger(__name__)
 
+    """Тестирование endpoint http://httpbin.org/headers"""
     def test_headers(self):
         link = "http://httpbin.org/headers"
         TestApi.LOGGER.info("Sending request: " + link)
         result = requests.get(link)
         TestApi.LOGGER.info("Getting response: " + str(result.json()))
-        assert 200 == result.status_code
+        try:
+            #проверяем, что вернулся status_code 200
+            assert 200 == result.status_code
+        except AssertionError as err:
+            TestApi.LOGGER.error(err, exc_info=True)
+            raise err
 
+    """Тестирование endpoint http://httpbin.org/status с параметром 200"""
     def test_status(self):
-        link = "http://httpbin.org/status/200"
+        link = "http://httpbin.org/status/300"
         TestApi.LOGGER.info("Sending request: " + link)
         result = requests.get(link)
         TestApi.LOGGER.info("Getting response: " + str(result.content))
-        assert 200 == result.status_code
+        try:
+            #проверяем, что вернулся status_code 200
+            assert 300 == result.status_code
+        except AssertionError as err:
+            TestApi.LOGGER.error(err, exc_info=True)
+            raise err
 
     def test_redirect(self):
+        """Тестирование endpoint http://httpbin.org/redirect/ с параметром 1"""
         link = "http://httpbin.org/redirect/1"
         TestApi.LOGGER.info("Sending request: " + link)
         result = requests.get(link)
         TestApi.LOGGER.info("Getting response: " + str(result.json()))
-        assert 302 == result.status_code
+        try:
+            #проверяем, что вернулся status_code 302
+            assert 302 == result.status_code
+        except AssertionError as err:
+            TestApi.LOGGER.error(err, exc_info=True)
+            raise err
